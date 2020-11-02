5b5ca088a72807c17fde3a0d7fdcedac28f48a0c
satya kavya <satyakavya24@gmail.com>
2020-11-02 15:20:04 -0600

    add pets table

--- Develop/public/js/signup.js
+++ Develop/public/js/signup.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
   // Getting references to our form and input
+  var firstName = $("input#first-name-input");
+  var lastName = $("input#last-name-input");
   var signUpForm = $("form.signup");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
@@ -8,6 +10,8 @@ $(document).ready(function() {
   signUpForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
+      firstName :firstName.val().trim(),
+      lastName :lastName.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
@@ -16,15 +20,19 @@ $(document).ready(function() {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.firstName, userData.lastName,userData.email, userData.password);
+    firstName.val("");
+    lastName.val("");
     emailInput.val("");
     passwordInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(firstName ,lastName ,email, password) {
     $.post("/api/signup", {
+      firstName:firstName,
+      lastName:lastName,
       email: email,
       password: password
     })
