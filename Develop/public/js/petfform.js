fd304c049eae1fe99b57e83a6302791eafa95b18
satya kavya <satyakavya24@gmail.com>
2020-11-03 11:55:31 -0600

    add routes to save pet details

--- Develop/public/js/petfform.js
+++ Develop/public/js/petfform.js
@@ -1,33 +1,59 @@
 
+$("#submit").on("click", function(event){
+  event.preventDefault();
+  const newPet ={
+      petName : $("#petName").val().trim(),
+      petAge :$("#petAge").val().trim(),
+      petBreed : $("#petBreed").val().trim(),
+      UserId : userid
+
+  }
+  let userId;
+  $.get("/api/user_data").then(function(data) {
+       userId = data.id;
+    console.log(data);
+  //   $(".member-name").text(data.firstName);
+  });
+
+
+  $.post("/api/pets",newPet)
+  .then(data=>{
+      console.log(data);
+      alert("adding new pet")
+  });
+ 
+
+  $("#petName").val("");
+  $("#petAge").val("");
+  $("#petBreed").val("");
 
 
 
-$(document).ready(function() {
-    petname,petage , petbreed
-var pet = {
-    petname: petname.val().trim(),
-    petnAge: petname.val().trim(),
+});
 
-    petnBreed: petname.val().trim(),
-    userId :userId,
+
+
+
+// $(document).ready(function() {
+//     petname,petage , petbreed
+// var pet = {
+//     petname: petname.val().trim(),
+//     petnAge: petname.val().trim(),
+
+//     petnBreed: petname.val().trim(),
+//     userId :userId,
       
-    complete: false
+//     complete: false
 
-  };
-    // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    let userId;
-    $.get("/api/user_data").then(function(data) {
-         userId = data.id;
-    //   console.log(data);
-    //   $(".member-name").text(data.firstName);
-    });
+//   };
+//     // This file just does a GET request to figure out which user is logged in
+//     // and updates the HTML on the page
+   
+//    $.post("/api/pets",pet).then(function(){
 
-   $.post("/api/pets",pet).then(function(){
-
-   })
+//    })
 
 
-  });   
+//   });   
 
 
