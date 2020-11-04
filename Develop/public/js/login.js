ccfce1c76595f9f3e025e138bf191a8e8e5e8af7
satya kavya <satyakavya24@gmail.com>
2020-11-04 13:06:12 -0600

    debug login page

--- Develop/public/js/login.js
+++ Develop/public/js/login.js
@@ -1,7 +1,5 @@
 $(document).ready(function() {
   // Getting references to our form and inputs
-  var firstName = $("input#first-name-input");
-  var lastName = $("input#last-name-input");
   var loginForm = $("form.login");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
@@ -10,27 +8,23 @@ $(document).ready(function() {
   loginForm.on("submit", function(event) {
     event.preventDefault();
     var userData = {
-      firstName : firstName.val().trim(),
-      lastName : lastName.val().trim(),
       email: emailInput.val().trim(),
       password: passwordInput.val().trim()
     };
 
-    if (!userData.email || !userData.password || !userData.firstName || !userData.lastName) {
+    if (!userData.email || !userData.password) {
       return;
     }
 
     // If we have an email and password we run the loginUser function and clear the form
-    loginUser(userData.email, userData.password, userData.email, userData.password);
+    loginUser(userData.email, userData.password);
     emailInput.val("");
     passwordInput.val("");
   });
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the members page
-  function loginUser(email, password, firstName, lastName) {
+  function loginUser(email, password) {
     $.post("/api/login", {
-      firstName: firstName,
-      lastName: lastName,
       email: email,
       password: password
     })
@@ -43,3 +37,19 @@ $(document).ready(function() {
       });
   }
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Develop/public/js/yourpets.js
+++ Develop/public/js/yourpets.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
                console.log(pets);
                pets.forEach((pet, index) => {
                     wellSection.append(renderPet({ pet, index }));
+                   
                })
           })
      }
@@ -24,9 +25,9 @@ $(document).ready(function () {
           const { pet, index } = props;
           const { id ,petName, petAge, petBreed } = pet;
           return $(/*html*/`
-       <div class="card-body" id=${id}>
+       <div class="card-body" data-id=${id}>
        <ul>
-       <div class="card text-white bg-info mb-12" style="max-width: 60rem;">
+       <div class="card text-white bg-info mb-12 shadow-lg rounded" style="max-width: 60rem;">
          <div class="card-body">
            
            <div class ="row">
@@ -39,35 +40,42 @@ $(document).ready(function () {
            <li class="list-inline-item" style="font-size:20px">Pet Breed : ${petBreed}</li>
            </div>
            <div class ="col-md-2">
-           <button type="button" class="btn btn-danger btn-sm" id ="delete"><span class="fas fa-trash"></span>Delete Pet</button>
+           <button type="submit" class="btn btn-danger btn-sm" id ="delete"  data-id = ${id}><span class="fas fa-trash"></span>Delete Pet</button>
              </div>
          </div>
           </div>
            </div>
              </ul>
         
-             </div>  `);
-
-
-
-
+             </div> 
+             `,
+            
              
-     }
-
-
+             
+             
+             
+             
+             );
+ }
      $("#delete").on("click", function(){
-          var id  = $("#id").val().trim();
-          console.log("this is and id of petid" +id);
-          $.ajax({
-               method: "DELETE",
-               url: "/api/pets/" + id
-             })
-               // On success, run the following code
-               .then(function() {
-                 console.log("Deleted Successfully!");
-               });
-           
-     })
+          var id;
+       console.log("this is and id of petid" +id);
+
+       id  = $(this).attr("data-id");
+       console.log("this is and id of petid-----" +id);
+
+       $.ajax({
+            method: "DELETE",
+            url: "/api/pets/" + id
+          })
+            // On success, run the following code
+            .then(function() {
+              console.log("Deleted Successfully!");
+            });
+        
+  })   
+
+  
 
      // $.delete("/api/pets/:id").then(function(data){
      //      console.log(data);
