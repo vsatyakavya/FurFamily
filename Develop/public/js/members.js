2adcb35b19a8477367ac11b41858c041bf3843b7
MarkA <m_aguilar_2011@yahoo.com>
2020-11-04 01:28:07 -0600

    added dog api to the members page

--- Develop/public/js/members.js
+++ Develop/public/js/members.js
@@ -4,4 +4,21 @@ $(document).ready(function() {
   $.get("/api/user_data").then(function(data) {
     $(".member-name").text(data.firstName);
   });
+
+  
+  $.ajax({
+    url: "https://api.thedogapi.com/v1/images/search/",
+    method: "GET"
+
+  }).then(function (response) {
+    console.log(response)
+    $('#dogName').text(response[0].breeds[0].name)
+    $('#origin').text(response[0].breeds[0].origin)
+    $('#dogTemperment').text(response[0].breeds[0].temperament)
+    $('#lifeSpan').text(response[0].breeds[0].life_span)
+
+  });
+  
+
+
 });
